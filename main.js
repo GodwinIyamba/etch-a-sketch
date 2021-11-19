eccfa580ea49409a97b32a9e1b1b8a7246ff742b
Godwin Iyamba <goediya@gmail.com>
2021-11-19 14:08:07 +0100

    Modify index.html, style.css and main.js

--- main.js
+++ main.js
@@ -1,45 +1,51 @@
 const container = document.querySelector('.grid-container');
-const clearBtn = document.querySelector('.clear-grid');
+const clearBtn = document.querySelector('.btn-clear');
+const userInput = document.querySelector('.user-choice');
 
 let gridTile = 16;
-let width = 340 / gridTile;
-let height = 340 / gridTile;
+let width = 540 / gridTile;
+let height = 540 / gridTile;
 let gridSize = gridTile * gridTile;
 
-
+//Initialize: Create grid squares and add event listener
 function initialize(squareSize, squareWidth, squareHeight) {
     for(let i = 0; i < squareSize; i++) {
     const tile = document.createElement('div');
     tile.classList.add('tile');
     tile.style.width = `${squareWidth}px`;
     tile.style.height = `${squareHeight}px`;
+    
     container.appendChild(tile);
 
-    tile.addEventListener('mouseover', (e) => e.target.classList.add('hover'))
-    }
+    tile.addEventListener('mouseover', (e) => {
+        e.target.classList.add('hover');
+        if(tile.classList.contains('hover')) {
+        let hsl = `hsl(${Math.floor(Math.random() * 100)}, 100%, 50%)`;
+        tile.style.backgroundColor = hsl;
+        
+        }
+    })} 
 }
 
+//Clear grid
 function clearGrid() {
-    do {
-        gridTile = prompt('Input number of square. Maximum square is 100: ')
-    } while (gridTile > 100 && gridTile);
-
+    gridTile = Number(userInput.value)
+    userInput.value = "";
+    
     const tiles = document.querySelectorAll('.tile');
     for(let i = 0; i < tiles.length; i++) {
         container.removeChild(tiles[i])
     }
     
-    width = 340 / gridTile;
-    height = 340/ gridTile;
+    width = 540 / gridTile;
+    height = 540/ gridTile;
     gridSize = gridTile * gridTile;
 
     initialize(gridSize, height, width);
-    console.log(width);
+    
 }
-console.log(width);
-
-
 
 window.addEventListener('click', initialize(gridSize, width, height));
 clearBtn.addEventListener('click', clearGrid);
+
 
