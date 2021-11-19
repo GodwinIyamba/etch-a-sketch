b216e4d6ddc194004ad60e0757ff795cab086ae3
Godwin Iyamba <goediya@gmail.com>
2021-11-19 11:42:31 +0100

    Implement a functioning etch-a-sketch

--- main.js
+++ main.js
@@ -1,7 +1,45 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('.grid-container');
+const clearBtn = document.querySelector('.clear-grid');
 
-for(let i = 0; i < 256; i++) {
+let gridTile = 50;
+let width = 340 / gridTile;
+let height = 340 / gridTile;
+let gridSize = gridTile * gridTile;
+
+
+function initialize(squareSize, squareWidth, squareHeight) {
+    for(let i = 0; i < squareSize; i++) {
     const tile = document.createElement('div');
     tile.classList.add('tile');
+    tile.style.width = `${squareWidth}px`;
+    tile.style.height = `${squareHeight}px`;
     container.appendChild(tile);
+
+    tile.addEventListener('mouseover', (e) => e.target.classList.add('hover'))
+    }
 }
+
+function clearGrid() {
+    do {
+        gridTile = prompt('Input number of square. Maximum square is 100: ')
+    } while (gridTile > 100 && gridTile);
+
+    const tiles = document.querySelectorAll('.tile');
+    for(let i = 0; i < tiles.length; i++) {
+        container.removeChild(tiles[i])
+    }
+    
+    width = 340 / gridTile;
+    height = 340/ gridTile;
+    gridSize = gridTile * gridTile;
+
+    initialize(gridSize, height, width);
+    console.log(width);
+}
+console.log(width);
+
+
+
+window.addEventListener('click', initialize(gridSize, width, height));
+clearBtn.addEventListener('click', clearGrid);
+
